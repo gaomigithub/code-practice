15b3edd26c0f8bfb3fcc956cdf919f2f3ebc4afd
Mi Gao <398938578@qq.com>
2022-11-28 20:05:48 +0800

    update

--- DataStructure/BinaryTree/preorderTraversal.js
+++ DataStructure/BinaryTree/preorderTraversal.js
@@ -15,7 +15,29 @@ const recursion = (root, arr = []) => {
   return arr
 }
 
+const rec = (root, arr = []) => {
+  if (root) {
+    arr.push(root.val)
+    rec(root.left, arr)
+    rec(root.right, arr)
+  }
+  return arr
+}
+
 const iteration = (root) => {
+  if (!root) return []
+  const res = []
+  const stack = [root]
+  while (stack.length) {
+    let temp = stack.pop()
+    res.push(temp.val)
+    if (temp.right) stack.push(temp.right) //  因为栈先进后出所以将right放前
+    if (temp.left) stack.push(temp.left)
+  }
+  return res
+}
+
+const _iteration = (root) => {
   const res = []
   const stack = []
   let curr = root
