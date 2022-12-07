037a1d4b7ed63c806a02c65e906dab7cccc977d4
Mi Gao <398938578@qq.com>
2022-12-07 18:32:25 +0800

    array unique

---
+++ arrayUnique.js
@@ -0,0 +1,29 @@
+export class Unique {
+  // 利用ES6: filter
+  byFilter(arr) {
+    const res = new Map()
+    return arr.filter((item) => !res.has(item) && res.set(item, 1))
+  }
+  byFilter2(arr) {
+    return arr.filter(function (item, index) {
+      return arr.indexOf(item) === index // indexOf只能查找到第一个
+    })
+  }
+
+  // 利用includes实现数组去重
+  byIncludes(arr) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+      if (!newArr.includes(arr[i])) {
+        newArr.push(arr[i])
+      }
+    }
+    return newArr
+  }
+
+  // Set结构不能接收重复数据
+  bySet(arr) {
+    const res = [...new Set(arr)]
+    return res
+  }
+}
